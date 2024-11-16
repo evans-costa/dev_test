cb0f3c8c59d6e7eceaf8aec429324e3fe8667da2
Evandro Costa <evandro.souzac@gmail.com>
2024-11-16 14:33:17 -0300

    use routes and adjust server start

--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,30 @@
 import 'reflect-metadata';
 import express from 'express';
-import { DataSource } from 'typeorm';
-import { User } from './entity/User';
-import { Post } from './entity/Post';
+import database from './infra/db';
+import userRoutes from './routes/userRoutes';
+import postRoutes from './routes/postRoutes';
 
 const app = express();
 app.use(express.json());
 
-const AppDataSource = new DataSource({
-  type: "mysql",
-  host: process.env.DB_HOST || "localhost",
-  port: 3306,
-  username: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "password",
-  database: process.env.DB_NAME || "test_db",
-  entities: [User,Post],
-  synchronize: true,
-});
-
-const wait = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
-
-const initializeDatabase = async () => {
-  await wait(20000);
+const startServer = async () => {
   try {
-    await AppDataSource.initialize();
-    console.log("Data Source has been initialized!");
-  } catch (err) {
-    console.error("Error during Data Source initialization:", err);
+    await database.initializeDatabase();
+    console.log('Database initialized');
+
+    const PORT = process.env.PORT || 3000;
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error('Error initializing the database:', error);
     process.exit(1);
-  }
-};
+  };
+}
 
-initializeDatabase();
+startServer();
 
-app.post('/users', async (req, res) => {
-// Crie o endpoint de users
-});
+app.use("/users", userRoutes)
+app.use("/posts", postRoutes)
 
-app.post('/posts', async (req, res) => {
-// Crie o endpoint de posts
-});
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
