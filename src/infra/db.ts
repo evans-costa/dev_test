c02188ed3bb9779708998720afec2c27dcc4c8bc
Evandro Costa <evandro.souzac@gmail.com>
2024-11-16 14:32:39 -0300

    create `/infra` folder to handle database initialization

---
+++ src/infra/db.ts
@@ -0,0 +1,35 @@
+import { DataSource } from 'typeorm';
+import { User } from '../entity/User';
+import { Post } from '../entity/Post';
+
+const AppDataSource = new DataSource({
+  type: "mysql",
+  host: process.env.DB_HOST || "localhost",
+  port: 3306,
+  username: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "password",
+  database: process.env.DB_NAME || "test_db",
+  entities: [User,Post],
+  logging: true,
+  synchronize: true,
+});
+
+const wait = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+
+const initializeDatabase = async () => {
+  await wait(20000);
+  try {
+    await AppDataSource.initialize();
+    console.log("Data Source has been initialized!");
+  } catch (err) {
+    console.error("Error during Data Source initialization:", err);
+    process.exit(1);
+  }
+};
+
+const database = {
+  AppDataSource,
+  initializeDatabase,
+}
+
+export default database;
